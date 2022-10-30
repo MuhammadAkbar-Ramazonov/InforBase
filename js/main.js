7037dcf50c4e23f413fa2ac7e80e221a2aab3ba5
John Doe <ramazonov3600@gmail.com>
2022-10-30 15:13:48 +0500

    add modal

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-var elOpenMenu = document.querySelector(".site-header__burger");
-var elOpenList = document.querySelector(".sitenav");
+var elOpenModalBtn = document.querySelector(".js-modal-btn");
+var elOpenModal = document.querySelector(".modal");
+var elCloseModal = document.querySelector(".js-close-modal-btn");
 var elBody = document.querySelector("body");
 var elItem = document.querySelectorAll(".test-hero__item");
 
-
 elItem.forEach(function(link){
-    
     link.addEventListener("click" , function(){    
         elItem.forEach(function(item){
             item.classList.remove("test-hero__item-active");
@@ -14,7 +13,6 @@ elItem.forEach(function(link){
     });
 });
 
-
 elItem.forEach(function(item){
     item.addEventListener("click" , function(){
         item.classList.add("test-hero__item-active");
@@ -22,8 +20,10 @@ elItem.forEach(function(item){
     
 });
 
-elOpenMenu.addEventListener("click", function(){
-    elBody.classList.toggle("body-overflow");
-    elOpenList.classList.toggle("sitenav--active");
-    elOpenMenu.classList.toggle("site-header__burger--close");
+elOpenModalBtn.addEventListener("click", function(){
+    elOpenModal.classList.add("modal-open");
+});
+
+elCloseModal.addEventListener("click", function(){
+    elOpenModal.classList.remove("modal-open");
 });
